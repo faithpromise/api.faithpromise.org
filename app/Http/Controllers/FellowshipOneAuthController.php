bc87f9f0612118fb20ecc65759c75fa60814498d
Brad Roberts <brad@bradroberts.net>
2015-10-29 13:49:29 -0400

    Added some comments

--- app/Http/Controllers/FellowshipOneAuthController.php
+++ app/Http/Controllers/FellowshipOneAuthController.php
@@ -9,7 +9,22 @@ use Illuminate\Routing\Controller as BaseController;
 
 class FellowshipOneAuthController extends BaseController {
 
+    /**
+     * Called by client (Satellizer) to get an OAuth request token
+     * from FellowshipOne
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function requestToken(Request $request) {
+
+//        die('test');
+
+        // This is what I want it to look like
+//        return response()->json([
+//            'oauth_token'    => $app->make('FellowshipOneService')->getRequestToken(),
+//            'oauth_callback' => url('/v1/auth/access-token') // TODO: Use named route
+//        ]);
 
         $key = env('F1_KEY');
         $secret = env('F1_SECRET');
@@ -26,6 +41,13 @@ class FellowshipOneAuthController extends BaseController {
 
     }
 
+    /**
+     * Successful FellowshipOne login will redirect back to our app
+     * and call this method.
+     *
+     * @param Request $request
+     * @throws \App\FaithPromise\F1\Exception
+     */
     public function accessToken(Request $request) {
 
         $key = env('F1_KEY');
