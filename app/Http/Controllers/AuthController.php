6a4538e0b87d32948d150f1b91954ff937e6100f
Brad Roberts <brad@bradroberts.net>
2015-11-02 13:12:36 -0500

    Added methods to register and verify email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,8 @@ use App\Http\Requests;
 use App\User;
 use FaithPromise\FellowshipOne\AuthFacade;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Mail;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
 
@@ -30,10 +32,62 @@ class AuthController extends Controller {
 
     }
 
+    public function register(Request $request) {
+
+        // Validate email
+        if (!preg_match('/@faithpromise\.org$')) {
+            throw new \Exception('Faith Promise email address required.');
+        }
+
+        $data = [];
+        $data['email'] = $request->input('email');
+        $data['verification_token'] = 'fp-' . str_random(16);
+        $data['verification_url'] = url('/verify-email?t=' . $data['verification_token']);
+
+        Cache::put($data['verification_token'], $data['email'], 5);
+
+        Mail::send('emails.register', ['data' => $data], function ($message) use ($data) {
+            $message->from('dev@faithpromise.org');
+            $message->to($data['email']);
+        });
+
+        return true;
+    }
+
+    public function verifyEmail(Request $request) {
+
+//        $payload = JWTAuth::parseToken()->getPayload();
+//        dd($payload->get(''));
+
+        if ($email = Cache::get($request->input('t'))) {
+
+            $payload = JWTAuth::parseToken()->getPayload();
+
+
+
+            // Create the user
+            $user = new User;
+            $user->email = $email;
+            $user->password = 'login_via_f1_' . str_random(35);
+            $user->fellowship_one_user_id = $payload->get('fellowship_one_user_id');
+            $user->save();
+
+
+            $new_token = JWTAuth::fromUser($user, [
+                'oauth_token'        => $auth['oauth_token'],
+                'oauth_token_secret' => $auth['oauth_token_secret']
+            ]);
+
+        }
+
+        return response()->json(['message' => 'Validation email expired.'], 404);
+
+    }
+
     private function handleRequestToken(Request $request) {
 
         return response()->json([
-            'oauth_token' => AuthFacade::obtainRequestToken(),
+            'oauth_token'    => AuthFacade::obtainRequestToken(),
             'oauth_callback' => $request->server('HTTP_REFERER')
         ]);
 
@@ -52,8 +106,9 @@ class AuthController extends Controller {
         try {
 
             $token = JWTAuth::fromUser($user, [
-                'oauth_token'        => $auth['oauth_token'],
-                'oauth_token_secret' => $auth['oauth_token_secret']
+                'oauth_token'            => $auth['oauth_token'],
+                'oauth_token_secret'     => $auth['oauth_token_secret'],
+                'fellowship_one_user_id' => $auth['user_id']
             ]);
 
             if (!$token) {
---
+++ resources/views/emails/register.html
@@ -0,0 +1,12 @@
+<p>Please click the following link to verify your email address with Faith Promise.</p>
+
+<p>{{ $data['verification_url'] }}</p>
+
+<p>If you received this email by mistake, simply ignore it.</p>
+
+<p>Thank you,</p>
+<p>
+  Faith Promise Church<br>
+  http://faithpromise.org<br>
+  865-251-2590
+</p>
