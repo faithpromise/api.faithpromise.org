6dfb96281951286485fa1c992b4294adcff78534
Brad Roberts <brad@bradroberts.net>
2015-11-03 16:40:34 -0500

    Refactoring

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,24 +34,31 @@ class AuthController extends Controller {
 
     public function register(Request $request) {
 
+        // Make sure username ends with '@faithpromise.org'
+        $email = preg_replace('/(?:@faithpromise\.org)+$/', '', $request->input('username')) . '@faithpromise.org';
+
         // Validate email
-        if (!preg_match('/@faithpromise\.org$')) {
-            throw new \Exception('Faith Promise email address required.');
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new \Exception('A valid Faith Promise email address is required to register.');
         }
 
         $data = [];
-        $data['email'] = $request->input('email');
+        $data['email'] = $email;
         $data['verification_token'] = 'fp-' . str_random(16);
-        $data['verification_url'] = url('/verify-email?t=' . $data['verification_token']);
+        $data['verification_url'] = config('site.admin_url') . '/verify-email/' . $data['verification_token'];
 
         Cache::put($data['verification_token'], $data['email'], 5);
 
+        // TODO: Remove return
+//        return $data['verification_url'];
+
         Mail::send('emails.register', ['data' => $data], function ($message) use ($data) {
-            $message->from('dev@faithpromise.org');
-            $message->to($data['email']);
+            $message
+                ->from('dev@faithpromise.org')
+                ->to($data['email'])
+                ->subject('Faith Promise: Verify your email address');
         });
 
-        return true;
     }
 
     public function verifyEmail(Request $request) {
@@ -59,25 +66,35 @@ class AuthController extends Controller {
 //        $payload = JWTAuth::parseToken()->getPayload();
 //        dd($payload->get(''));
 
-        if ($email = Cache::get($request->input('t'))) {
+        $email = Cache::get($request->input('token'));
+
+        if ($email) {
 
             $payload = JWTAuth::parseToken()->getPayload();
 
+            $user = User::whereEmail($email)->first();
+
             // Create the user
-            $user = new User;
-            $user->email = $email;
-            $user->password = 'login_via_f1_' . str_random(35);
+            if (!$user) {
+                $user = new User;
+                $user->email = $email;
+                $user->password = 'login_via_f1_' . str_random(35);
+            }
+
             $user->fellowship_one_user_id = $payload->get('fellowship_one_user_id');
             $user->save();
 
-            $new_token = JWTAuth::fromUser($user, [
+            $token = JWTAuth::fromUser($user, [
                 'oauth_token'        => $payload->get('oauth_token'),
                 'oauth_token_secret' => $payload->get('oauth_token_secret')
             ]);
 
+            // TODO: Limit what is returned back in user object
+            return response()->json(compact('token', 'user'));
+
         }
 
-        return response()->json(['message' => 'Validation email expired.'], 404);
+        return response()->json(['message' => 'Validation email expired: ' . $email], 404); // TODO:
 
     }
 
@@ -92,23 +109,27 @@ class AuthController extends Controller {
 
     private function handleAccessToken(Request $request) {
 
+        $result = [];
         $auth = AuthFacade::obtainAccessToken($request->input('oauth_token'));
         $user = User::whereFellowshipOneUserId($auth['user_id'])->first();
+        $claims = [
+            'oauth_token'            => $auth['oauth_token'],
+            'oauth_token_secret'     => $auth['oauth_token_secret'],
+            'fellowship_one_user_id' => $auth['user_id']
+        ];
 
-        if (!$user) {
+        if ($user) {
+            $result['user'] = $user->toArray();
+        } else {
             $user = new User();
             $user->id = 0;
         }
 
         try {
 
-            $token = JWTAuth::fromUser($user, [
-                'oauth_token'            => $auth['oauth_token'],
-                'oauth_token_secret'     => $auth['oauth_token_secret'],
-                'fellowship_one_user_id' => $auth['user_id']
-            ]);
+            $result['token'] = JWTAuth::fromUser($user, $claims);
 
-            if (!$token) {
+            if (!$result['token']) {
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
 
@@ -116,7 +137,7 @@ class AuthController extends Controller {
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
 
-        return response()->json(compact('token', 'user'));
+        return response()->json($result);
 
     }
 
